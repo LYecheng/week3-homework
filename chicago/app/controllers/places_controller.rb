bd686343647349fd2600d22e454a49707fbc0131
Yecheng Li <lyc@uchicago.edu>
2015-04-18 23:56:33 -0500

    delete done

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -5,11 +5,13 @@ class PlacesController < ApplicationController
 	end
 
 	def show
-		
+		@place = Place.find_by(:id => params["id"])
 	end
 
 	def delete
-		
+		@entry = Place.find_by(:id => params["id"])
+		@entry.destroy
+		redirect_to "/places"
 	end
 
 	def new
